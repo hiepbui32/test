e90bb8c3d5e6d1b3c9740b96a8c896836c3090b5
hiepbui32 <hiepbui2812@gmail.com>
2023-05-15 20:28:06 +0700

    Some changes

--- test/Github.cs
+++ test/Github.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,8 @@ namespace test
     internal class Github
     {
         int a;
+        int b;
+        int c;
+
     }
 }
